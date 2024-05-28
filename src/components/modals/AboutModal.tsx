b1525984d94906bb0e1acad77b6d10a95c9d9654
Bareket <bareket.damari@live.biu.ac.il>
2024-05-28 03:41:23 +0300

    chore: Update AboutModal component and tailwind.css
    This commit updates the AboutModal component by adding a profile image and updating the content. It also adjusts the CSS in tailwind.css to change the position of the modal. These changes improve the visual appearance and user experience of the AboutModal component.

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -1,21 +1,28 @@
 import { JSX } from "solid-js";
+import profileImage from "~/assets/images/profile.png"; // Adjust the path as necessary
 
-const AboutModal = (): JSX.Element => (
-  <>
-    <div class="mb-4">
-      <img src="" alt="" class="w-full rounded-lg shadow-lg" />
-    </div>
-    <div>
-      <h1 class="text-3xl mb-2">Ruth Thomson</h1>
-      <p>
-        I'm a front-end developer. I have dedicated myself to creating visually
-        appealing and user-friendly websites. I'm always open to using new skill
-        sets and I believe in the power of collaboration, working closely with
-        designers and back-end developers to deliver cohesive and effective
-        solutions.
-      </p>
-    </div>
-  </>
-);
+const AboutModal = (): JSX.Element => {
+  return (
+    <>
+      <div class="mb-4 bg-gray-100 flex items-center justify-center w-20 h-50 ">
+        <img
+          src={profileImage}
+          alt="Just A profile Photo"
+          class="max-h-20 rounded-lg shadow-lg  transform hover:scale-125 transition duration-500 ease-in-out"
+        />
+      </div>
+      <div>
+        <h1 class="text-3xl mb-2">Holy me</h1>
+        <p>
+          I'm a front-end developer. I have dedicated myself to creating
+          visually appealing and user-friendly websites. I'm always open to
+          using new skill sets and I believe in the power of collaboration,
+          working closely with designers and back-end developers to deliver
+          cohesive and effective solutions.
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default AboutModal;
