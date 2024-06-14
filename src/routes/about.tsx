3615257b539d5ba15c1ebeca48dd0d6211e18c93
Bareket <bareket.damari@live.biu.ac.il>
2024-06-14 10:09:58 +0300

    adding about

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,10 +1,26 @@
 import { Title } from "@solidjs/meta";
+import EmojiAnimator, { EmojiArray } from "~/components/EmojiAnimator";
 
 export default function Home() {
+  let sequence: EmojiArray = {
+    name: "Train Sequence",
+    description: "A sequence representing a train ride.",
+    emojis: ["🚂", "🚂🚃", "🚂🚃🚃", "🚂🚃🚃",
+      "🚂🚃🚃🚃", "🚂🚃🚃🚃🚃", "🚂🚃🚃🚃🚃🚃", "🚂🚃🚃🚃🚃🚃🚃",
+      "🚃🚃🚃🚃🚃🚃🚃💨", " 🚃🚃🚃🚃🚃🚃 💨", "     🚃🚃🚃🚃🚃 💨", "     🚃🚃🚃🚃  💨",
+      "    🚃🚃🚃  💨", "     🚃🚃  💨", "       🚃 💨 ", "         💨 ",
+      "         💨💨 ", "     💨   💨 ", "  🚨   ", "   🚨    "],
+    size: "2em",
+    duration: 3000,
+  };
   return (
+
     <main>
       <Title>About</Title>
       <h1>About</h1>
+      <div class="p-3 bg-gray-200 rounded-full mb-4 mt-2 ">
+        <EmojiAnimator emojis={sequence.emojis} size={sequence.size} duration={sequence.duration} />
+      </div>
     </main>
   );
 }
