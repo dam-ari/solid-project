fc8ca6a88f3bc6310f5f58e0c19389bb1d7fc801
Bareket <bareket.damari@live.biu.ac.il>
2024-06-23 16:42:46 +0300

    responsivity

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,51 +1,56 @@
 import { For, JSX, createSignal } from "solid-js";
 
 const Carousel = (props: { images: { src: string; alt: string; }[] }): JSX.Element => {
-    let carouselRef: HTMLDivElement;
-    const [currentIndex, setCurrentIndex] = createSignal(0);
+  let carouselRef: HTMLDivElement;
+  const [currentIndex, setCurrentIndex] = createSignal(0);
 
-    const nextImage = () => {
-        if (currentIndex() < props.images.length - 1) {
-            setCurrentIndex(currentIndex() + 1);
-            scrollToCurrentImage();
-        }
-    };
+  const nextImage = () => {
+    if (currentIndex() < props.images.length - 1) {
+      setCurrentIndex(currentIndex() + 1);
+      scrollToCurrentImage();
+    }
+  };
 
-    const prevImage = () => {
-        if (currentIndex() > 0) {
-            setCurrentIndex(currentIndex() - 1);
-            scrollToCurrentImage();
-        }
-    };
+  const prevImage = () => {
+    if (currentIndex() > 0) {
+      setCurrentIndex(currentIndex() - 1);
+      scrollToCurrentImage();
+    }
+  };
 
-    const scrollToCurrentImage = () => {
-        const scrollAmount = carouselRef.scrollWidth / props.images.length;
-        carouselRef.scrollTo({
-            left: scrollAmount * currentIndex(),
-            behavior: "smooth",
-        });
-    };
+  const currentImage = (index: number) => {
+    setCurrentIndex(index);
+    scrollToCurrentImage();
+  };
 
-    return (
-        <div class="relative max-w-30 max-w-md mx-auto p-4 rounded-box">
-            <div
-                ref={carouselRef!}
-                class="carousel-container overflow-x-scroll overflow-y-hidden inline-flex max-w-full rounded-box no-scrollbar"
-            >
-                <div class="carousel flex">
-                    <For each={props.images}>
-                        {(img) => (
-                            <div class="carousel-item flex-shrink-0 w-full p-2 px-4 rounded-3xl card">
-                                <img src={img.src} alt={img.alt} class="rounded-xl w-full object-cover p-1 glass-effect" />
-                            </div>
-                        )}
-                    </For>
-                </div>
-            </div>
-            <button class="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full" onClick={prevImage}>‹</button>
-            <button class="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full" onClick={nextImage}>›</button>
-            <style>
-                {`
+  const scrollToCurrentImage = () => {
+    const scrollAmount = carouselRef.scrollWidth / props.images.length;
+    carouselRef.scrollTo({
+      left: scrollAmount * currentIndex(),
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div class="relative max-w-30 max-w-md mx-auto p-4 rounded-box">
+      <div
+        ref={carouselRef!}
+        class="carousel-container overflow-x-scroll overflow-y-hidden inline-flex max-w-full rounded-box no-scrollbar"
+      >
+        <div class="carousel flex">
+          <For each={props.images}>
+            {(img, i) => (
+              <div class="carousel-item flex-shrink-0 w-full p-2 px-4 rounded-3xl card" onClick={() => currentImage(i())}>
+                <img src={img.src} alt={img.alt} class="rounded-xl w-full p-1 glass-effect" />
+              </div>
+            )}
+          </For>
+        </div>
+      </div>
+      <button class="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full" onClick={prevImage}>‹</button>
+      <button class="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full" onClick={nextImage}>›</button>
+      <style>
+        {`
           .card {
             align-content: space-around;
             background: linear-gradient(90deg, #ff005282, #0265a1db);
@@ -65,6 +70,12 @@ const Carousel = (props: { images: { src: string; alt: string; }[] }): JSX.Eleme
             }
           }
 
+          .glass-effect {
+            backdrop-filter: blur(10px);
+            background-color: rgba(255, 255, 255, 0.1);
+            border-radius: 10px;
+            border: 1px solid rgba(255, 255, 255, 0.2);
+          }
 
           .no-scrollbar::-webkit-scrollbar {
             height: 8px;
@@ -84,9 +95,9 @@ const Carousel = (props: { images: { src: string; alt: string; }[] }): JSX.Eleme
             scrollbar-color: transparent transparent;
           }
         `}
-            </style>
-        </div>
-    );
+      </style>
+    </div>
+  );
 };
 
 export default Carousel;
